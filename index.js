fc18482c3c54cd71b52d528fe3dce1fc64b2eac2
sukey <sdklos@gmail.com>
2017-10-07 13:28:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.reduce(function (agg, el, i, arr) {
+    console.log(el.name);
+  });
+}
+
+function logDriversByHometown(drivers, place) {
+  drivers.forEach(function (el, i, arr) {
+    if (el.hometown === place) {
+      console.log(el.name);
+    };
+  });
+}
+
+function driversByRevenue(drivers) {
+  const safeDrivers = [...drivers];
+  const newArray = safeDrivers.sort(function (a, b) {
+    return a.revenue - b.revenue
+  });
+  return newArray;
+}
+
+function driversByName(drivers) {
+  const safeDrivers = [...drivers];
+  const newArray = safeDrivers.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+    return newArray;
+}
+
+function totalRevenue(drivers) {
+  return drivers.reduce(revenueCallback, 0);
+};
+
+const revenueCallback = function(agg, el, i, arr) {
+  return agg + el.revenue;
+};
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
