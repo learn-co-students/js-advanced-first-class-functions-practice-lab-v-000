f60615b8c381026bbfb4b8b5dbc4933b3e22a312
smithwebtek <brad@smithwebtek.com>
2017-10-09 06:05:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const peeps = [
-  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
-  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
-  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
-  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
-  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
-];
-
 const logDriverNames = function (obj){
   for (let el of obj){
     console.log(el.name)
