4dcf04a63bbf16a9599220907f1a001822a78dcc
CLackrone <charisaanne@gmail.com>
2017-10-12 12:52:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name);
+    }
+  });
+}
+
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (first, second) {
+    return first.revenue - second.revenue;
+  });
+}
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (first, second) {
+    return first.name.localeCompare(second.name);
+  });
+}
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (totalRevenue, driver) {
+    return driver.revenue + totalRevenue;
+  }, 0);
+}
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
+
