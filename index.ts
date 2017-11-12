4590fad08d29157d222eca150ada5b70be8b1695
kaisa <kaisa.piipari@gmail.com>
2017-11-12 10:35:35 +0000

    Done.

--- index.ts
+++ index.ts
@@ -1,33 +1,44 @@
 function logDriverNames(drivers) {
     drivers.forEach(driver => console.log(driver.name));
 }
-let driverTowns = function (town) {
+
+let driverTowns = function(town: string) {
     return driver => {
         if (driver.hometown === town) {
             console.log(driver.name);
         }
     };
 };
-function logDriversByHometown(drivers, town) {
+
+function logDriversByHometown(drivers: any[], town: string) {
     drivers.forEach(driver => {
         if (driver.hometown === town) {
             console.log(driver.name);
         }
     });
 }
-function driversByRevenue(drivers) {
+
+interface Driver {
+    revenue: number;
+    name: string;
+    hometown: string;
+}
+
+function driversByRevenue(drivers: Driver[]): Driver[] {
     return drivers.slice().sort((a, b) => a.revenue - b.revenue);
 }
-function driversByName(drivers) {
+
+function driversByName(drivers: Driver[]): Driver[] {
     return drivers.slice().sort((a, b) => a.name.localeCompare(b.name));
 }
-function totalRevenue(drivers) {
+
+function totalRevenue(drivers: Driver[]): number {
     return drivers.map(driver => driver.revenue).reduce((a, b) => {
         return a + b;
     });
 }
-function averageRevenue(drivers) {
+
+function averageRevenue(drivers: Driver[]): number {
     debugger;
     return totalRevenue(drivers) / drivers.length;
 }
-//# sourceMappingURL=index.js.map
