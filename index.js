fd8a049ebd48c51c5dff4f79ce31337608e995bb
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-19 00:13:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const logDriverNames = function (drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function(driver){
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
+}
+
+const driversByRevenue = function (drivers) {
+  return [...drivers].sort(function(driverA, driverB){
+    return driverA.revenue - driverB.revenue;
+  });
+}
+
+const driversByName = function (drivers) {
+  return [...drivers].sort(function(driverA, driverB) {
+    return driverA.name.localeCompare(driverB.name);
+  });
+}
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (agg, el, i, arr){
+    return agg + el.revenue;
+  }, 0);
+}
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
