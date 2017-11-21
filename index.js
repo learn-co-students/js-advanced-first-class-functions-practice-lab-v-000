92382f74468754e1eeca31294359d88288200d01
Elias Ventura <eliasventura23@gmail.com>
2017-11-21 23:31:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// Code your solution in this file!
-const logDriverNames = function (drivers) {
-  drivers.forEach(function (driver) {
-    console.log(driver.name);
+let logDriverNames = (drivers) => {
+  return drivers.forEach(function (driver) {
+    console.log(driver.name)
   })
 }
 
-const logDriversByHometown = function (drivers, location) {
-  drivers.forEach(function (driver) {
+let logDriversByHometown = (drivers, location) => {
+  return drivers.forEach(function (driver) {
     if(driver.hometown === location) {
       console.log(driver.name);
     }
   })
 }
 
-const driversByRevenue = function (drivers) {
-  const driversCopy = drivers.slice()
-  const sortedByRevenue = driversCopy.sort(function (a, b) {
+let driversByRevenue = (drivers) => {
+  let copyOfDrivers = [...drivers]
+  return copyOfDrivers.sort(function (a, b) {
     return a.revenue - b.revenue;
   })
-  return sortedByRevenue;
 }
 
-const driversByName = function (drivers) {
-  const driversCopy = drivers.slice()
-  return driversCopy.sort(function (a,b) {
+const driversByName = (drivers) => {
+  let copyOfDrivers = [...drivers]
+  return copyOfDrivers.sort(function (a, b) {
     return a.name.localeCompare(b.name);
   })
 }
 
-const totalRevenue = function (drivers) {
-  const reduceTotalRevenue = function (agg, el, i, drivers) {
-    return agg + el.revenue;
-  }
-  return drivers.reduce(reduceTotalRevenue, 0);
+const totalRevenue = (drivers) => {
+  return drivers.reduce(cbRevenue, 0)
 }
 
-const averageRevenue = function (drivers) {
-  return totalRevenue(drivers)/drivers.length
+function cbRevenue(agg, el, i, array) {
+  return agg + el.revenue;
+  }
+
+
+let averageRevenue= (drivers) => {
+  return totalRevenue(drivers)/drivers.length;
 }
