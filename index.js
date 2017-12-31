ef699d7c446b9a64b739c602ef87db850c62e45d
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-12-31 01:43:16 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+  for (const driver of drivers) {
+  console.log(driver.name);
+  }
+};
+
+const logDriversByHometown = function (drivers, selected_hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === selected_hometown) {
+      console.log(driver.name);
+    }
+  });
+};
+
+const driversByRevenue = function (drivers)  {
+  const driversCopy = drivers.slice();
+  return driversCopy.sort(function(a,b) {
+    return a.revenue - b.revenue;
+  });
+};
+
+const driversByName = function (drivers) {
+  const driversCopy = drivers.slice();
+  return driversCopy.sort(function(a,b) {
+    return a.name.localeCompare(b.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers)/drivers.length;
+};
