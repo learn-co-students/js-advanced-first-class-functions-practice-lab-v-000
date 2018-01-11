54053b4580b5ae74a8658b7c4965b05a9c487ef0
streamedLine <dovidlwtn@gmail.com>
2018-01-11 18:36:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+	for (const driver of drivers) {
+		console.log(driver.name)
+	}
+}
+
+function logDriversByHometown(drivers, hometown) {
+	return logDriverNames(drivers.filter(driver => driver.hometown === hometown))
+}
+
+function driversByRevenue(drivers) {
+	return drivers.slice(0).sort(function(a, b){
+		return a.revenue - b.revenue
+	});
+}
+
+function driversByName(drivers) {
+	return drivers.slice(0).sort(function(a, b){
+		return a.name.localeCompare(b.name)
+	});
+}
+
+function totalRevenue(drivers) {
+	return drivers.reduce(function(agg, driver){
+		return agg += driver.revenue;
+	}, 0);
+}
+
+function averageRevenue(drivers) {
+	return totalRevenue(drivers) / drivers.length;
+}
