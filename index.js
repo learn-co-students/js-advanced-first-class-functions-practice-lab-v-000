54721953a36719493c35a657546ed3312186c74b
Oski <github email address>
2018-02-06 08:00:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+    drivers.forEach(function(driver){
+        return console.log(driver.name)
+    })
+}
+
+function logDriversByHometown(drivers, location){
+    drivers.forEach(function(driver){
+        if (driver.hometown === location)
+            return console.log(driver.name)
+    })
+}
+
+function driversByRevenue(drivers){
+    return drivers.slice().sort(function(a,b){
+        return a.revenue - b.revenue
+    })
+}
+
+function driversByName(drivers){
+    return drivers.slice().sort(function(a,b){
+        return a.name.localeCompare(b.name)
+    })        
+}
+
+const totalRevenue = function(drivers){
+    return drivers.reduce(function(total, currentDriver){
+        return total + currentDriver.revenue;
+    }, 0)
+}
+
+const averageRevenue = function(drivers){
+    return drivers.reduce(function(average, currentDriver){
+        let total = 0;
+        return average += (total + currentDriver.revenue) / drivers.length
+    }, 0);
+};
