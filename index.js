35253da278d75c65f3b2d4e53bee8e3b9b202e8a
Alexander Friedland <friedlandaj@gmail.com>
2018-02-12 23:53:18 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ const logDriverNames = function (array) {
     console.log(driver.name);
   });
 };
+
+const logDriversByHometown = function (array, location) {
+  array.forEach (function (driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name);
+    };
+  });
+};
+
+const driversByRevenue = function (array) {
+  return array.slice().sort(function (driver1, driver2) {
+    return driver1.revenue - driver2.revenue;
+  });
+}
+
+const driversByName = function (array) {
+  return array.slice().sort(function (driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name);
+  });
+};
+
+const totalRevenue = function (array) {
+  return array.reduce(function (total, driver) {
+    return driver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
