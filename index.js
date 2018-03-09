b8d0d418254b628bf27de1504f02423e9b6660c4
John Fewell <fewell@gmail.com>
2018-03-09 21:53:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const logDriverNames = function (arr){
+  arr.forEach(function (driver) {
+    console.log(driver.name);
+  })
+}
+
+
+const logDriversByHometown = function (arr, location){
+    arr.forEach(function (driver){
+      if (driver.hometown === location){
+        console.log(driver.name);
+    };
+  });
+};
+
+const driversByRevenue = function (arr, revenue){
+  const arrCopy = arr.slice()
+  return arrCopy.sort(function (driver1, driver2){
+    return driver1.revenue - driver2.revenue;
+  });
+};
+
+const driversByName
