c223e612cc4f0de9e67400a6311b200acf4ae2fa
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-03-12 15:59:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Code your solution in this file!
+
 const logDriverNames = function (drivers) {
   drivers.forEach(function (driver) {
     console.log(driver.name);
@@ -11,4 +11,26 @@ const logDriversByHometown = function (drivers, hometown) {
       console.log(driver.name);
     }
   });
+};
+
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
+};
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
 };
