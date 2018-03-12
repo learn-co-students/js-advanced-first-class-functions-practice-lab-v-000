02e0887d9273960c4f52cc886a7ae9045c21f884
Morgan Fudge <mfudge92@gmail.com>
2018-03-12 01:38:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function logDriverNames(names) {
+  for(el in names) {
+    console.log(names[el].name)
+  }
+}
+
+function logDriversByHometown(names, town) {
+  for(el in names) {
+    if(names[el].hometown == town) { console.log(names[el].name) }
+  }
+}
+
+function driversByRevenue(arr) {
+  let newArr = [...arr]
+  return newArr.sort(function (a, b) {
+    return a.revenue > b.revenue
+  })
+}
+
+function driversByName(arr) {
+  let newArr = [...arr]
+  return newArr.sort(function (a, b) {
+    return a.name.localeCompare(b.name)
+  })
+}
+
+function totalRevenue(arr) {
+  return arr.reduce((a, c) => { return { revenue : a.revenue + c.revenue } }).revenue
+}
+
+function averageRevenue(arr) {
+  return arr.reduce((a, c) => { return { revenue : a.revenue + c.revenue } }).revenue / arr.length
+}
