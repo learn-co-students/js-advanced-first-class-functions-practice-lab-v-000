48cb8476d8a3a440564920be1fd5ca84761a6a21
BuffaloRob <peckham.rob@gmail.com>
2018-03-28 13:40:12 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  for (const element of drivers) {
+      console.log(element.name);
+  }
+};
+
+function logDriversByHometown(drivers, location) {
+  for (const element of drivers) {
+    if (element.hometown === location) {
+      console.log(element.name)
+    }
+  }
+};
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort(function(a,b){
+    return a.revenue - b.revenue;
+  });
+};
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function (a,b) {
+    return a.name.localeCompare(b.name);
+  });
+};
+
+const totalRevenue = function(drivers) {
+  return drivers.reduce(function(total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
