ee12b070cb73b867995783665736bb8836ff8fc7
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-04-02 01:25:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function logDriverNames(list){
+  for (const driver of list){
+    console.log(driver.name);
+  };
+};
+
+function logDriversByHometown(list, hometown){
+  let newList = list.filter(function(driver){return driver.hometown===hometown})
+  for (const driver of newList){
+    console.log(driver.name);
+  };
+};
+
+function driversByRevenue(list) {
+  return list.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
+};
+
+function driversByName(list){
+  return list.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+function totalRevenue(list) {
+  return list.reduce(function (total, driver) {
+    return driver.revenue + total;
+  }, 0);
+};
+
+function averageRevenue(list) {
+  return totalRevenue(list) / list.length;
+};
