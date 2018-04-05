35fd958d95d4ecf20e8058541ac9f8ec1dc30010
ultimatecrispy <ultimatecrispy@gmail.com>
2018-04-05 12:12:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function logDriverNames(drivers){
+  return drivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, location){
+  return drivers.forEach(function(driver){
+    driver.hometown === location ? console.log(driver.name) : null;
+  });
+}
+
+function driversByRevenue(drivers){
+  return drivers.slice().sort(function(driverA, driverB){
+    return driverA.revenue > driverB.revenue;
+  });
+}
+
+function driversByName(drivers){
+  return drivers.slice().sort(function(driverA, driverB){
+    return driverA.name > driverB.name;
+  });
+}
+
+function totalRevenue(drivers){
+  return drivers.reduce(
+    function(total, driver){
+      return total + driver.revenue;
+    },0);
+}
+
+const averageRevenue = drivers => totalRevenue(drivers)/drivers.length;
