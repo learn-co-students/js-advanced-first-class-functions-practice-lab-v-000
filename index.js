2bea9f18e6c1f30a5fd45486e25d5149421e0e08
Robert Laws <roblaws@me.com>
2018-04-06 20:40:14 +0300

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(element) {
+    console.log(element.name);
+  })
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function(element) {
+    if (element.hometown == location) {
+      console.log(element.name);
+    }
+  })
+}
+
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
+};
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+function totalRevenue(drivers) {
+  let totalRevenue = 0
+  drivers.forEach(function(element) {
+    totalRevenue += element.revenue;
+  });
+  return totalRevenue;
+}
+
+function averageRevenue(drivers) {
+  let totalRevenue = 0;
+  let counter = 0;
+  drivers.forEach(function(driver) {
+    totalRevenue += driver.revenue;
+    counter++;
+  });
+  let averageRevenue = totalRevenue / counter;
+  return averageRevenue;
+}
