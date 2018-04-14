a6aaf7e3bcab37f43a220cd0e44917992e8cd162
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 14:19:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach((driver) => console.log(driver.name));
+}
+
+function logDriversByHometown(drivers, hometown) {
+  const matched = drivers.filter((driver) => hometown === driver.hometown);
+  logDriverNames(matched);
+}
+
+function driversByRevenue(drivers) {
+  return drivers.slice(0).sort((a,b) => a.revenue-b.revenue);
+}
+
+function driversByName(drivers) {
+  return drivers.slice(0).sort((a,b) => a.name.localeCompare(b.name));
+}
+
+function totalRevenue(drivers) {
+  return drivers.reduce((total, driver) => total + driver.revenue,0);
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers)/drivers.length;
+}
