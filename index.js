b430f47a148c76bd86d746b00734b0d5a69c4888
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-29 20:04:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  for (const element of drivers) {
+    console.log(element.name);
+  }
+}
+
+function logDriversByHometown(drivers, location){
+  for (const element of drivers) {
+    if (element.hometown === location){
+      console.log(element.name);
+    }
+  }
+}
+
+function driversByRevenue(drivers){
+  const compare = function (a, b) {
+    return a.revenue - b.revenue
+  }
+  return [...drivers].sort(compare)
+}
+
+function driversByName(drivers){
+  const compare = function (a, b) {
+    return a['name'].localeCompare(b['name']);
+  }
+  return [...drivers].sort(compare)
+}
+
+function totalRevenue(drivers){
+  const reduceRevenue = function (agg, el, i, arr) {
+    return agg + el.revenue;
+  }
+  return drivers.reduce(reduceRevenue, 0);
+}
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers)/(drivers.length)
+}
