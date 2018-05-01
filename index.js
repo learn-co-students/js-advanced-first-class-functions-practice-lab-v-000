a7a2a169291e6a0bf8077738ebc119909ea2c86e
saurookadook <maskiella@gmail.com>
2018-04-30 20:21:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames (drivers) {
+  return drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown (drivers, ht) {
+  return drivers.forEach(function (driver) {
+    if (driver.hometown === ht) {
+      console.log(driver.name);
+    };
+  });
+}
+
+function driversByRevenue (drivers) {
+  return drivers.slice().sort(function (driverA, driverB){
+    return driverA.revenue - driverB.revenue;
+  });
+}
+
+function driversByName (drivers) {
+  return drivers.slice().sort(function (driverA, driverB) {
+    return driverA.name.localeCompare(driverB.name);
+  });
+}
+
+function totalRevenue (drivers) {
+  return drivers.reduce(function (total, driver) {
+    return driver.revenue + total;
+  }, 0);
+}
+
+function averageRevenue (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
