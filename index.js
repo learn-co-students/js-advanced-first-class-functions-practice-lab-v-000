81a9893c8f072ff21f34a12a7f305459e2ce69d5
jacobsilver2 <jacobsilver2@mac.com>
2018-05-05 11:55:35 -0400

    work on step 3

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const logDriversCallback = function (el, i, arr) {
+  console.log(el.name);
+}
+
+function logDriverNames(driver) {
+  driver.forEach(logDriversCallback);
+}
+
+
+
+function logDriversByHometown(driver, location) {
+  driver.forEach(function(d, i, arr) {
+    if (d.hometown === location) {
+      console.log(d.name);
+    }
+  });
+}
+
+function driversByRevenue(driver) {
+  const sorted = driver.sort(driver.revenue)
+}
