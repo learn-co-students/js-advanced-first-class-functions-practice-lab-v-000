d719fdc601cfc4fae7e20317fed11f1689528da0
Randall Robinson <keith_adams007@yahoo.com>
2018-05-21 02:23:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const first = function (el, i, arr) {
+  console.log(el.name)
+}
+
+
+function logDriverNames(array) {
+  array.forEach(first)
+}
+
+
+function logDriversByHometown(array, location) {
+  array.forEach(function (el, i, arr) {
+    if (location === el.hometown) {
+      console.log(el.name)
+    }
+  })
+}
+
+function driversByRevenue(array) {
+  array.sort(function (a, b) {
+  return a.revenue - b.revenue 
+  })
+}
+
+function driversByName(array) {
+  array.sort(function (a, b){
+    return a.name.localeCompare(b.name)
+  })
+}
+
+function totalRevenue(array) {
+  
+}
