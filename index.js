3f2930054cab5705744a96bf7172b202a24ff6ec
funemployeddad <funemployeddad@gmail.com>
2018-05-26 15:23:43 -0700

    need to go back and review

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+function logDriverNames(list) {
+  function(name){
+    console.log(name)
+  });
+}
+//filter by key = name then console log each element in the array
