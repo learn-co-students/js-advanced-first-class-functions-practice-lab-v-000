60e3842c0c69a5b8d65bc6fb2fd11cfc82b242c2
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-05-28 11:53:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+
+function logDriverNames(arr) {
+  arr.forEach(function(driver) {
+    console.log(driver.name);
+  })
+}
+
+function logDriversByHometown(arr, location) {
+  arr.forEach(function(driver) {
+    if (driver.hometown === location)
+      console.log(driver.name);
+  })
+}
+
+function driversByRevenue(arr) {
+  const newDrivers = arr.slice();
+  return newDrivers.sort(function(a,b) {
+    return a.revenue - b.revenue;
+  })
+}
+
+function driversByName(arr) {
+  const newDrivers = arr.slice();
+  return newDrivers.sort(function(a, b) {
+    return (a.name).localeCompare(b.name);
+  })
+}
+
+function totalRevenue(arr) {
+  let revenue = 0;
+  arr.forEach(function(driver) {
+    revenue += driver.revenue
+  })
+  return revenue;
+}
+
+function averageRevenue(arr) {
+  let revenue = 0;  
+  arr.forEach(function(driver) {
+    revenue += driver.revenue
+  })
+  return (revenue / arr.length);
+}
