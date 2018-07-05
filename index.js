8aa0fac968fb045ada156eb78b259bb15c9a6dc7
funemployeddad <funemployeddad@gmail.com>
2018-07-05 14:37:04 -0700

    in progress

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // Code your solution in this file!
 
 function logDriverNames(list) {
-  function(name){
-    console.log(name)
+  return list.map(function(driver){
+    console.log(driver.name);
   });
 }
 //filter by key = name then console log each element in the array
+// do the filter by hometown, then call logDriverNames on that array
+
+function logDriversByHometown (list, hometown) {
+  return logDriverNames( list.filter(function (driver){
+    return driver.hometown === hometown;
+    })
+  );
+}
+
+function driversByRevenue (list) {
+  return logDriverNames (list.sort(function (driver){
+    return driver.revenue;
+  }));
+}
