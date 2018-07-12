eb6d498328c261b8c92365c7dfd2e84d4b2a35f1
Emily Jirles <emily.jirles@gmail.com>
2018-07-12 18:30:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+
+function logDriverNames(driversArray) {
+  driversArray.forEach(function(element){ console.log(element.name);});
+};
+
+function logDriversByHometown (driversArray, hometown) {
+    driversArray.forEach(
+      function (element) {
+        if (element.hometown === hometown) {
+          console.log(element.name);
+        }
+      }
+    );
+  };
+
+function driversByRevenue(driversArray){
+  const result = [...driversArray];
+  result.sort(
+    function(obj1, obj2) {
+      return obj1.revenue - obj2.revenue;
+    }
+  );
+  return result;
+};
+
+function driversByName(driversArray){
+  const result = [...driversArray];
+  result.sort(
+    function(obj1, obj2) {
+      return obj1.name.localeCompare(obj2.name);
+    }
+  );
+return result;
+};
+
+function totalRevenue(driversArray) {
+  const cb = function (agg, el, i, arr) {
+    return agg += el.revenue;
+  };
+  return driversArray.reduce(cb, 0);
+};
+
+function averageRevenue(driversArray) {
+  const total =  totalRevenue(driversArray);
+  return total / driversArray.length;
+};
