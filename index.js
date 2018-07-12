c61ca8490724726106c9af98a43073185fa5f895
github username <github email address>
2018-07-11 19:30:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // Code your solution in this file!
 
+//use .map to console.log each driver's name element
  const logDriverNames = function (list) {
   return list.map(function(driver){
     console.log(driver.name);
   });
 }
 
+//use .filter to return true if the hometown passed in the argument is equal to the value of the hometown key stored in the driver's object
  const logDriversByHometown = function (list, hometown) {
   return logDriverNames( list.filter(function (driver){
     return driver.hometown === hometown;
     })
   );
 }
+
 
  const driversByRevenue = function (list) {
    return list.slice().sort(function (driverOne, driverTwo){
