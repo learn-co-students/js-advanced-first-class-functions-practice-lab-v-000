777e0fed93467e5ae41883b3cc91b99d8317f7fb
katleiahramos <katleiahramos@gmail.com>
2018-07-12 22:14:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(
+    function(driver) {
+      console.log(driver.name)})
+}
+
+function logDriversByHometown(drivers, value) {
+  drivers.forEach( function(driver) { if (driver.hometown == value){
+      console.log(driver.name)
+  } })
+}
+
+function driversByRevenue(drivers){
+  drivers.sort( function (driver) {driver.revenue})
+}
