7fdf115d0a76d0924ea69f4fcfd756887a3efcd8
katleiahramos <katleiahramos@gmail.com>
2018-07-17 15:21:28 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,29 @@ function logDriversByHometown(drivers, value) {
 }
 
 function driversByRevenue(drivers){
-  drivers.sort( function (driver) {driver.revenue})
+  const newArr = drivers.slice().sort( function (a,b) {
+    return a.revenue - b.revenue;
+  })
+  return newArr
+}
+
+function driversByName(drivers) {
+  const newArr = drivers.slice().sort( function(a,b) {
+    return a.name.localeCompare(b.name)
+  }
+  )
+  return newArr
+}
+
+const sumRevenue = function (agg, el, i, arr) {
+  return agg + el.revenue
+}
+function totalRevenue(drivers) {
+  const newArr = drivers.slice().reduce( sumRevenue,0)
+  return newArr
+
+}
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length
 }
