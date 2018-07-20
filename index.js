cdb800675f4e2e78b1a4c3aeb222f61f6c082475
Rygel-XVI <micha232@umn.edu>
2018-07-20 08:49:33 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const logDriverNames = function(array) {
+  array.forEach(function(item) {console.log(item.name)});
+}
+
+const logDriversByHometown = function(array, str) {
+  array.forEach(function(item) {
+    if (str === item.hometown) {
+      console.log(item.name);
+    }
+  });
+}
+
+const driversByRevenue = function(array) {
+   const newArray = array.sort(function(a, b) {
+    return a.revenue - b.revenue;
+  })
+  return newArray
+}
+
+const driversByName = function(array) {
+
+}
+
+const totalRevenue = function(array) {
+
+}
+
+const averageRevenue = function(array) {
+
+}
