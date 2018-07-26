1e5c47729f8edc44544806ce96d976c0d6c0770d
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-26 13:04:09 -0700

    first few functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const logDriverNamesCallback = function (el, i, arr){
+    console.log(el.name);
+};
+
+function logDriverNames(drivers){
+    drivers.forEach(logDriverNamesCallback);
+};
+
+
+const logDriversByHometownCallback = function (el, i, arr){
+    if (el.hometown === location){
+       return console.log(el.name);
+    }
+};
+
+function logDriversByHometown(drivers,location){
+    drivers.forEach(logDriversByHometownCallback);
+}
