8172349b01e7f5e61f9112eb5660762a08944f6c
Breyanna Vass <breyannavass@gmail.com>
2018-08-02 19:09:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function logDriverNames(a_drivers) {
+    a_drivers.map( (value) => {console.log(value["name"]) } )
+}
+
+function logDriversByHometown(a_drivers, hometown) {
+    for(const driver of a_drivers) {
+        console.log((driver["hometown"] == hometown) ? driver["name"] : "")
+    }
+}
+
+function driversByRevenue (a_drivers) {
+    let arr = Object.assign([], a_drivers)
+    return arr.sort( (a,b) => {return (a["revenue"] < b["revenue"]) ? -1 : ((a["revenue"] > b["revenue"]) ? 1 : 0) } )
+}
+
+function driversByName (a_drivers) {
+    let arr = Object.assign([], a_drivers)
+    return arr.sort( (a,b) => {return (a["name"] < b["name"]) ? -1 : ((a["name"] > b["name"]) ? 1 : 0) } )
+}
+
+function totalRevenue(a_drivers) {
+    return a_drivers.reduce( (sum, obj) => { return sum += obj["revenue"]}, 0)
+}
+
+function averageRevenue(a_drivers) {
+    return a_drivers.reduce( (sum, obj) => { return sum += obj["revenue"]}, 0 ) / a_drivers.length
+}
