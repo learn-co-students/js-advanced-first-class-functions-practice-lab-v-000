9e0caebb94ff537d5dc3282f3e6cb60d3a784574
Diego Yanez <diegoyanez@MacBook-Pro-4.local>
2018-08-06 14:21:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+const logDriverNames = function(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name)
+  });
+};
+
+
+function logDriversByHometown(drivers, town){
+    drivers.forEach(function(driver){
+      if (driver.hometown === town) {
+        console.log(driver.name)
+      };
+    });
+};
+
+const driversByRevenue = function(drivers){
+    return drivers.slice().sort(function(d1, d2){
+      return d1.revenue - d2.revenue
+    })
+}
+
+const driversByName = function(drivers){
+    return drivers.slice().sort(function(a, b){
+      return a.name.localeCompare(b.name);
+    })
+}
+
+const totalRevenue = function(drivers){
+    return drivers.reduce(function(total, currentDriver){
+      return currentDriver.revenue + total;
+    }, 0)
+}
+
+const averageRevenue = function(drivers){
+  return totalRevenue(drivers) / drivers.length
+}
