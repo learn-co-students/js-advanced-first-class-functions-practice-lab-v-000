889552b3430a33dc49d75dc5b8fd2c3f95dc24fc
learn <eric.desimone@cortland.edu>
2018-08-16 07:41:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver){
+  console.log(driver.name);
+  });
+};
+
+function logDriversByHometown(drivers, location){
+  drivers.forEach(function(driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name);
+    };
+  });
+};
+
+function driversByRevenue(drivers) {
+  const driversCopy = [...drivers];
+  driversCopy.sort(function(driver1, driver2) {
+      return driver1.revenue - driver2.revenue;
+  });
+  return driversCopy
+};
+
+function driversByName(drivers) {
+  const driversCopy = [...drivers];
+  driversCopy.sort(function(driver1, driver2){
+    return driver1.name.localeCompare(driver2.name);
+  });
+  return driversCopy;
+};
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function(value, driver) {
+    return value + driver.revenue;
+  }, 0);
+};
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
