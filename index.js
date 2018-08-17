72440b708ae058ae15f75b0d3a8177399485d97d
cbshields <cbshields@outlook.com>
2018-08-17 16:44:47 -0700

    working on lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(drivers,location) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name)
+    }
+  })
+
+function driversByRevenue(drivers) {
+  
+}
