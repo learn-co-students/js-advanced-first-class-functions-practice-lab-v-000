1471ad5b775c859b232f9d2ce3e6410778b5de9e
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-18 19:37:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (driver) {
+    return console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, loc) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === loc) {
+      return console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
