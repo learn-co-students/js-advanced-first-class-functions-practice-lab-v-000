28f7effd0fa254a5670bb04fc338b8f28f48c4e5
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 20:37:46 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Receives an array of driver objects and logs the name attribute of each driver to the console.
 const logDriverNames = function (drivers) {
-  drivers.forEach(function (driver) {
+  drivers.forEach( function (driver) {
     console.log(driver.name);
   });
 };
@@ -11,7 +11,7 @@ const logDriverNames = function (drivers) {
 
 // Receives an array of driver objects as the first argument and a location as the second argument. The function logs to the console the name attribute of each driver whose hometown matches the string passed in as the 'location' argument.
 const logDriversByHometown = function(drivers, hometown) {
-  drivers.forEach(function (driver) {
+  drivers.forEach( function (driver) {
    if (driver.hometown === hometown) {
      console.log(driver.name);
    }
@@ -23,7 +23,7 @@ const logDriversByHometown = function(drivers, hometown) {
 const driversByRevenue = function (drivers) {
   const orderRevenues = [...drivers];
 
-  return orderRevenues.sort(function (a, b) {
+  return orderRevenues.sort( function (a, b) {
     return a.revenue - b.revenue;
   })
 };
@@ -32,7 +32,15 @@ const driversByRevenue = function (drivers) {
 const driversByName = function (drivers) {
   const orderNames = [...drivers];
 
-  return orderNames.sort(function (a, b) {
+  return orderNames.sort( function (a, b) {
     return (a.name).localeCompare(b.name);
   })
+};
+
+
+// Receives an array of driver objects and returns the sum of the revenue earned by each driver
+const totalRevenue = function (drivers) {
+  return drivers.reduce( function (total, driver) {
+    return total + driver.revenue; }, 0
+  );
 };
