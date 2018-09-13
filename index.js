8e1bf51681726f733a9d9e51b544f02f1e54c329
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-13 11:34:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(callback)
+}
+
+const callback = function (e) {
+  console.log(e.name)
+}
+
+function logDriversByHometown(drivers, city) {
+  for (const element of drivers) {
+    if (element.hometown === city) {
+      console.log(element.name)
+    }
+  }
+}
