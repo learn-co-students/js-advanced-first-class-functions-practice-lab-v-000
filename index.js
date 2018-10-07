ebe0f0a1aa3343cc4fd54b5da3b9cb59fdfe918d
hkleindl <hkleindl@gmail.com>
2018-10-07 18:33:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+};
+
+function logDriversByHometown(drivers, hometown) {
+  return drivers.filter(function(driver){
+    return driver.hometown === hometown;
+  }).filter(function(driver) {console.log(driver.name);
+  });
+};
+
+function driversByRevenue(drivers) {
+  let driversClone = [...drivers];
+  return driversClone.sort(function(a, b) {
+    return a.revenue - b.revenue; });
+};
+
+function driversByName(drivers) {
+  let driversClone = [...drivers];
+  return driversClone.sort(function(a, b){
+    return a.name.localeCompare(b.name)});
+  };
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function(agg, el, i, arr) {
+    return agg + el.revenue;}, 0);
+};
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
+
