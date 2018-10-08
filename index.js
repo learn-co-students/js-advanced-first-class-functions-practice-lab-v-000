88b0cc227f9a6bd353c292d70b0eb3a7b35cc472
Brittany <britt.cumens@gmail.com>
2018-10-08 13:44:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(driver => console.log(driver.name))
+};
+
+function logDriversByHometown(drivers, town) {
+  drivers.forEach( element => ( if element.hometown === town { console.log(element.name) } ) )
+};
