b6f011f5a6c014b4f6af6c5138e0b916c5cc01a3
ElleeB <laura.trager@gmail.com>
2018-10-18 14:14:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === location){
+      console.log(driver.name)
+    }
+  })
+}
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort(function (driverA, driverB) {
+     return driverA.revenue - driverB.revenue
+  })
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function (driverA, driverB) {
+    return driverA.name.localeCompare(driverB.name)
+  })
+}
+
+const drivers = [
+  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+]
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total
+  }, 0)
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers)/drivers.length
+}
