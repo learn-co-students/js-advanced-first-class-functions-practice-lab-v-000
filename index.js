134c2cc47400d2e78569e76aa1838213f204d1ed
Carol Greene <carolgreene3@gmail.com>
2018-10-26 21:51:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
 // Code your solution in this file!
-const logDriverNames = function (drivers) {
-  drivers.forEach(function (driver) {
-    console.log(driver.name);
-  });
-}
-
-const logDriversByHometown = function (drivers, location) {
-  drivers.forEach(function (driver) {
-    if (driver.hometown === location) {
-    console.log(driver.name)
-  }
+const logDriverNames = function(drivers) {    //set variable name to function w/driver argument
+  drivers.forEach(function(driver) {          //take array and iterate to give individual driver
+    console.log(driver.name)                  //console.log individual driver name
   })
 }
 
-const driversByRevenue = function (drivers) {
-  return drivers.slice().sort(function(driverA, driverB) {
-    return driverA.revenue - driverB.revenue;
-  });
+const logDriversByHometown = function(drivers, location) {   //set variable name to function w/driver array & location argument
+  drivers.forEach(function(driver) {                        //iterate through arr to get to indiv driver
+    if(driver.hometown === location)                        // look for drivers that have hometown same as location argument
+    console.log(driver.name)                                //console.log those matching driver's names
+  })
 }
 
-const driversByName = function (drivers) {
-  return drivers.slice().sort(function (driverA, driverB) {
-    return driverA.name.localeCompare (driverB.name);
-  });
+const driversByRevenue = function(drivers) {                //set variable to function w/driver array argument
+  return drivers.slice().sort(function(driver1, driver2) {     //use slice() to create dupl array & sort 2 drivers at a time
+    return driver1.revenue - driver2.revenue                  // put in order by subtractin driver 2 from driver 1
+  })
 }
 
-const totalRevenue = function (drivers) {
-  let totalRevenue = 0;
-  drivers.forEach(function (driver) {
-    totalRevenue += driver.revenue;
-  });
-  return totalRevenue;
+const driversByName = function(drivers) {                      //set variable to function w/driver array argument
+  return drivers.slice().sort(function(driver1, driver2) {     //use slice() to create dupl array & sort 2 drivers at a time
+    return driver1.name.localeCompare(driver2.name)            //compare driver1 name & driver2 name using localeCompare
+  })                                                           //which will make it case insensitive
 }
 
-const averageRevenue = function (drivers) {
-  return totalRevenue(drivers) / drivers.length;
+const totalRevenue = function(drivers) {                      //set variable to function w/driver array argument
+  return drivers.reduce(function(total, currentDriver) {     //use reduce to add up totals taking the total so far and currentDriver
+    return currentDriver.revenue + total                     // add currentDriver's rev to the total so far
+  }, 0)                                                       //need to check on this, got confused on this function
 }
+
+const averageRevenue = function(drivers) {                    //set var to function w/driver array argument
+  return totalRevenue(drivers) / drivers.length               // take totalRevenue(driver) function from above & divide by number
+}                                                             //of drivers for average
