55a805e57625551370b3e7fb86ec12df22356d98
Anu Khambete <anukhambete@gmail.com>
2018-11-15 16:22:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const logDriverNames = function (array) {
+  array.forEach(function(el, i, arr) {
+    console.log(el.name);
+  });
+}
+
+const logDriversByHometown = function (array, hometown) {
+  let new_array = array.filter(x => x.hometown == hometown);
+  new_array.forEach(function(el, i, arr) {
+    console.log(el.name);
+  });
+}
+
+const driversByRevenue = function (array) {
+  let new_array = [...array];
+    return new_array.sort(function (num1, num2) {
+    return num1.revenue - num2.revenue;
+  });
+}
