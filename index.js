cb8a388e0dae54bdcc2dff99c2a6ffc30b2ad019
Great_Divide <bbirdwell+git@gmail.com>
2018-12-20 13:06:40 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+// function logDriverNames(drivers) {
+// 	for (d of drivers) {
+// 		console.log(d.name)
+// 	}
+// }
+
+function logDriverNames(drivers) {
+	drivers.forEach(function(d) {
+		console.log(d.name);
+	});
+}
+
+
+function logDriversByHometown(drivers, loc) {
+	drivers.forEach(function(d) {
+		if (d.hometown === loc) {
+			console.log(d.name)
+		}
+	});
+}
+
+function driversByRevenue(drivers) {
+	return drivers.slice().sort(function (a, b) {
+		return a.revenue - b.revenue;
+	});
+}
+
+function driversByName(drivers) {	
+	return drivers.slice().sort(function (a, b) {
+		return a.name.localeCompare(b.name);
+	});
+}
+
+const totalRevenue = function(drivers) {
+	return drivers.reduce(function(total, driver) {
+		return total + driver.revenue
+	}, 0)
+}
+
+const averageRevenue = function(drivers) {
+	return totalRevenue(drivers)/drivers.length;
+}
+
