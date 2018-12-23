09ad4dc7909edd2185983a2c151a8c6e0efcbbc8
Kachel Wittig <kjwittig@gmail.com>
2018-12-23 03:19:16 -0600

    Donezo

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+const logDriverNames = function (arr) {
+  return arr.forEach(function (el) {
+    console.log(el.name)
+  })
+}
+
+const logDriversByHometown = function (driversArr, city) {
+  return driversArr.forEach(function (el) {
+    if (el.hometown === city) {
+      console.log(el.name)
+    }
+  })
+}
+
+const driversByRevenue = function (arr) {
+  return [...arr].sort(function (a, b) {
+      return a.revenue > b.revenue
+  })
+}
+
+const driversByName = function (arr) {
+  return [...arr].sort(function (a, b) {
+      return a.name > b.name
+  })
+}
+
+const revenueSum = function (agg, el, i, arr) {
+  return agg + el.revenue
+}
+
+const totalRevenue = function (arr) {
+  return arr.reduce(revenueSum, 0)
+}
+
+const averageRevenue = function (arr) {
+  return totalRevenue(arr) / arr.length
+}
