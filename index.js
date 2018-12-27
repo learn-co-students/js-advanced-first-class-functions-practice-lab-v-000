853f612784318dbb6c70a9eacca06ade83539cd8
epic0448 <samuelogundiran@Yahoo.com>
2018-12-26 23:58:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+// array1.forEach(function(element) {
+//     console.log(element);
+//   });
+
+//   function driversWithRevenueOver(drivers,revenue) {
+//     return drivers.filter(function(driver) {
+//           return (driver.revenue >= revenue)
+//     })
+// }
+
+function logDriverNames(drivers) {
+    drivers.forEach(function(driver){
+        console.log(driver.name)
+    });
+}
+
+function logDriversByHometown(drivers, hometown) {
+     drivers.filter(function(driver) {
+    return driver.hometown == hometown
+    }).forEach(function(driver) {
+      console.log(driver.name)
+      })
+  
+    }
