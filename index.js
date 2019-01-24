4b65bcfe320807c3e9235242d7331fae38f27fe1
BrentBauman86 <brentbauman86@gmail.com>
2019-01-24 15:30:36 -0800

    Done.

--- index.js
+++ index.js
@@ -16,16 +16,28 @@ const logDriversByHometown = function (drivers, hometown){
 };
 
 const driversByRevenue = function (drivers){
-    return drivers.sort(function (lowest, highest){
+    return drivers.slice().sort(function (lowest, highest){
         return lowest.revenue - highest.revenue 
     });
 };
 
 const driversByName = function (drivers){
-    return drivers.sort(function (first, last){
+    return drivers.slice().sort(function (first, last){
         return first.name.localeCompare(last.name);
     });
 };
+
+const totalRevenue = function (drivers){
+    var initialValue = 0;
+    return drivers.reduce(function (sum, driver){
+        return driver.revenue + sum;
+    }, initialValue);
+};
+
+const averageRevenue = function (drivers){
+        return totalRevenue(drivers) / drivers.length;
+};
+
 
 
 
