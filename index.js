fdaac65cc25f7130e1b874ec825a6bf5df6bf502
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-08 23:27:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const logDriverNames = function(array) {
+  array.forEach(function(element) {
+    console.log(element.name);
+  });
+}
+
+const logDriversByHometown = function(array, hometown) {
+  array.forEach(function(element) {
+    if (element.hometown === hometown) {
+      console.log(element.name);
+    };
+  });
+};
+
+const driversByRevenue = function(array) {
+  return [...array].sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+};
+
+const driversByName = function(array) {
+  return [...array].sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+};
+
+const totalRevenue = function (array) {
+  return array.reduce(function(total, element) {
+    return total + element.revenue;
+  }, 0);
+};
+
+const averageRevenue = function(array) {
+  return totalRevenue(array) / array.length;
+}
