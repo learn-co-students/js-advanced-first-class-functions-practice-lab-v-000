502c24892cc5e5e4b597c21dc096cf923db7d4f9
Pamela Frieze <pfrieze@gmail.com>
2019-02-11 01:43:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Code your solution in this file!
-const logDriverNames = function (driver, drivers) {
-    console.log(driver.name);
-};
+const logDriverNames = function (drivers) {
+    drivers.forEach(function(driver) {
+      console.log(driver.name)
+    })
+}
 
-drivers.forEach(logDriverNames);
+const logDriversByHometown = function(drivers, location) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === location) {
+    console.log(driver.name) }
+  })
+}
