7233ab6b75daa8f8e1d387bf35eba80ccb08aa32
Christine Cua <cloudycua@gmail.com>
2019-02-14 00:01:22 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 //   { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
 //   { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
 //   { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
-//   { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
-//   { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
 // ];
-
 
 // Logs the name attribute of each driver to the console.
 const logDriverNames = function (drivers) {
