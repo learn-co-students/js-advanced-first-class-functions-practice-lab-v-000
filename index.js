4c86d4aea1602f443cb083cc256713ae6377661d
Michael Dilley <mike.dilley@gmail.com>
2019-02-22 15:00:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  array.forEach(function(object){ console.log(object.name);});
+}
+
+function logDriversByHometown(array, location) {
+  array.forEach(function(object){
+    if (object.hometown === location) {
+      console.log(object.name);
+    }
+  });
+}
+
+function driversByRevenue(array){
+  return [...array].sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+function driversByName(array){
+  return [...array].sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(array){
+  let total = 0;
+  array.forEach(function(object){
+    total += object.revenue;
+  });
+  return total;
+}
+
+function averageRevenue(array){
+  return totalRevenue(array)/array.length;
+}
