5bb123e62698138419e7244ce9d333004e31fbf9
ryanwevans <ryanwevans@icloud.com>
2019-02-24 16:42:00 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  return array.forEach( d => {
+    console.log(d.name);
+  });
+};
+
+function logDriversByHometown(array, location) {
+  return array.forEach( d => {
+     if (d.hometown === location) {
+      console.log(d.name);
+    };
+  });
+};
+
+function driversByRevenue(array) {
+  return array.slice().sort( function(a, b) { return parseFloat(a.revenue) - parseFloat(b.revenue) } );
+};
+
+function driversByName(array) {
+  return array.slice().sort( function(a, b) { return a.name.localeCompare(b.name) } );
+};
+
+function totalRevenue(array) {
+  return array.reduce(
+  ( accumulator, currentValue ) => accumulator + currentValue.revenue, 0 );
+};
+
+function averageRevenue(array) {
+  const sum = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.revenue, 0 );
+    return sum/array.length;
+};
