861dfc109ee197b99f31f2144981f6f965caea88
Rachel Wyatt <rw126@evansville.edu>
2019-04-16 18:36:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+    
+function logDriverNames(drivers){
+  drivers.forEach((driver)=> {
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(drivers, location){
+  drivers.forEach((driver)=> {
+    if (location === driver.hometown) {
+    console.log(driver.name)
+    }
+  })
+}
+
+function driversByRevenue(drivers){
+  return drivers.slice(0).sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+function driversByName(drivers) {
+    return drivers.slice(0).sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(drivers){
+  const obj = drivers.reduce((a, b) => ({revenue: a.revenue + b.revenue}));
+  return obj.revenue;
+}
+
+function averageRevenue(drivers) {
+  const obj = drivers.reduce((a, b) => ({revenue: a.revenue + b.revenue}));
+  return obj.revenue / drivers.length;
+}
