eb7a5790ad4106175e4ce3f504f5430f53ba0d23
Ludmilla Aires Negri <ludaires@gmail.com>
2019-04-16 15:01:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function logDriverNames(obj){
+  obj.forEach(function(el){
+    console.log(el.name);
+  });
+}
+
+function logDriversByHometown(drivers, location){
+  drivers.forEach(function (el){
+    if (el.hometown === location) {
+      return console.log(el.name);     
+    };
+  });
+}
+
+function driversByRevenue(drivers){
+  const newDrivers = drivers.slice();
+  return newDrivers.sort(function (a, b){
+    return a.revenue - b.revenue;
+  }); 
+}
+
+function driversByName(drivers){
+  const newDrivers = drivers.slice();
+  return newDrivers.sort(function (a, b){
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(drivers){
+  return drivers.reduce(function (accumulator, a){
+    return accumulator + a.revenue;
+  }, 0);
+}
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
