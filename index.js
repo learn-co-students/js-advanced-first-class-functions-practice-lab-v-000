9e49fcca1408e2ebd6ecb072f09d27a9fc85edce
sjwakean <swakeman@ca.rr.com>
2019-05-24 11:30:05 -0700

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,38 @@ function logDriversByHometown(drivers, hometown) {
 };
 
 function driversByRevenue(drivers) {
-    drivers.sort(function(a, b) {
+    return drivers.slice().sort(function(a, b) {
         return a.revenue - b.revenue;
     });
-};
+ };
+
+ function driversByName(drivers) {
+    return drivers.slice().sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        };
+        if (nameA > nameB) {
+          return 1;
+        };
+        // names must be equal
+        return 0;
+    });
+ };
+
+function totalRevenue(drivers) {
+    
+    const revenueArray = drivers.map(driver => driver.revenue)
+    console.log(revenueArray)
+    const reducer = (currentValue, driver.revenue) => { return currentValue + revenue ;}
+    //console.log(drivers.reduce(reducer));
+}
+
+//function totalRevenue(drivers) {
+//const totalRevenue = (drivers.revenue + totalRevenue)
+//return drivers.reduce((revenue, totalRevenue) => {
+    //return totalRevenue + drivers.revenue;
+//});
+//};
+
