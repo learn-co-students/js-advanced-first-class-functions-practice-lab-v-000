fd8a7c0185cfc94c34a61323b043feadd1e12df6
Ashley <ashleywong678@gmail.com>
2019-06-05 13:47:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers){
+  return drivers.forEach(function(driver){
+    console.log(`${driver.name}`);
+  });
+}
+
+function logDriversByHometown(drivers, location){
+  return drivers.forEach(function(driver){
+    if (driver.hometown === location){
+      console.log(`${driver.name}`);
+    }
+  });
+}
+
+const driversByRevenue = function (drivers){
+  const copy = [...drivers]
+  const sortIt = function (d1, d2){
+    return d1.revenue - d2.revenue;
+  };
+  return copy.sort(sortIt);
+}
+
+function driversByName(drivers){
+  const copy = [...drivers]
+  const sortIt = function (d1, d2){
+    return d1.name.localeCompare(d2.name);
+  };
+  return copy.sort(sortIt);
+}
