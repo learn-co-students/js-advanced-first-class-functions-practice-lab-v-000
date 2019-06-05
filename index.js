737ae020ebdc793716823c364f7f5c10deb60904
sarawall1187 <sarawall1187@gmail.com>
2019-06-05 09:54:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function logDriverNames(drivers){
+    drivers.forEach(function(driver){
+        console.log(`${driver.name}`);
+    });
+}
+
+
+function logDriversByHometown(drivers, hometown){
+    drivers.forEach(function(driver){
+        if (driver.hometown === hometown)
+            console.log(`${driver.name}`);
+    });
+}
+
+function driversByRevenue(drivers){
+    const sortedDrivers = function(driver1, driver2){
+    return driver1['revenue'] - driver2['revenue'];
+    };
+    return drivers.sort(sortedDrivers);
+}
+
+
+// const numberSorter = function (num1, num2) {
+//     return num1 - num2;
+//   };
+   
+//   primes.sort(numberSorter);
