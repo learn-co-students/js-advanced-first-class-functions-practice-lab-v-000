75b5cc088a8b2fc74f91da7384ce331e037c89a2
yuanxizhang <yuanxi.zhang@gmail.com>
2019-06-24 00:00:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution in this file!
+function logDriverNames(drivers){
+	drivers.forEach(function(element) {
+  	console.log(element.name);
+	});
+}
+
+function logDriversByHometown(drivers, location) {
+	drivers.forEach(function(driver) {
+		if (driver.hometown === location) {
+			console.log(driver.name);
+		}
+	})
+}
+
+function driversByRevenue(drivers){
+	let sortedDrivers = drivers.slice(0);
+    
+	const revenueSorter = function (d1, d2) {
+  	return d1.revenue - d2.revenue;
+	};
+
+ 	sortedDrivers.sort(revenueSorter);
+ 	return sortedDrivers;
+}
+
+function driversByName(drivers){
+	let sortedDrivers = drivers.slice(0);
+
+	const comparator = function (a, b) {
+  	return a.name.localeCompare(b.name);
+	};
+
+	sortedDrivers.sort(comparator);
+	return sortedDrivers;
+}
+
+function totalRevenue(drivers){
+	return drivers.reduce((sum, d) => sum + d.revenue, 0);
+}
+
+function averageRevenue(drivers){
+	let total = totalRevenue(drivers);
+	let count = drivers.length;
+	return total/count;
+}
+	
+
+
+
+
+
+
+
+
+
+
+
