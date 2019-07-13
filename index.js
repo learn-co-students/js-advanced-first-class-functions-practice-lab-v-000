a2fdb61088225859b02068f2821d7ee0db1f526c
github username <github email address>
2019-07-13 17:22:53 -0400

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ function callback(el) {
   console.log(el.name);
 };
 
+function aCallback(el) {
+  console.log("top");
+  console.log(el.hometown);
+  console.log("bottom");
+};
+
 const logDriverNames = function(drivers){drivers.forEach(callback)};
+
+const logDriversByHometown = function(drivers,hometown){drivers.forEach(aCallback)};
