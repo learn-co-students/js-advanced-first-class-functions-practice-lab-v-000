e697d38ee53c2f49557029804ab604600581ae9b
Jason Mei <jason.mei403@gmail.com>
2019-09-13 22:19:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+const logDriverNames = function(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers, location) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name)
+    }
+  });
+};
+
+const driversByRevenue = function(drivers) {
+  const sortableDrivers = [...drivers];
+
+  sortableDrivers.sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+  return sortableDrivers;
+};
+
+const driversByName = function(drivers) {
+  const sortableDrivers = [...drivers];
+  sortableDrivers.sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  return sortableDrivers;
+};
+
+const totalRevenue = function(drivers) {
+  drivers.reduce(function(agg, el, i, arr) {
+    console.log('The aggregate up to this point is:', agg);
+    console.log('The index of the current element is:', i);
+    console.log("The current element's revenue is:", el.revenue);
+    console.log('----------------');
+    console.log(agg + el.revenue);
+  }, 0);
+};
